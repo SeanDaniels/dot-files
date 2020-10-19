94098ad4392212dcf979bd41c8acfea2203fe63c
seandaniels <sdaniel5@ncsu.edu>
2020-10-19 19:33:33 -0400

    Added emacs dot files

---
+++ emacs/custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(conda-anaconda-home "/home/swd/anaconda3/")
+ '(conda-env-home-directory "/home/swd/anaconda3/")
+ '(default-frame-alist
+    '((right-divider-width . 1)
+      (vertical-scroll-bars)
+      (tool-bar-lines . 0)
+      (menu-bar-lines . 0)
+      (width . 80)
+      (height . 55)))
+ '(initial-frame-alist '((height . 55) (top . -1) (width . 120)))
+ '(lsp-vhdl-server 'hdl-checker)
+ '(verilog-linter "verilator"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
